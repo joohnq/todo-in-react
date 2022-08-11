e1a0ccfc05207994243b39fd18871026379ae8f3
João Henrique <joaohenrique7482@gmail.com>
2022-08-11 19:01:11 -0300

    refactor: Style

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,36 +1,38 @@
 import React from "react";
-import './style.css'
+import "./style.css";
 
-export function List(props){
-
-    function DoneImg(props){
-        if(props.done){
-            return (
-                <img alt="Check Done" src="./check-enabled.png"></img>
-            )
-        }else{
-            return(
-            <img alt="Check Off" src="./check-disabled-sf.png"></img>
-            )
-        }
+export function List(props) {
+  function DoneImg(props) {
+    if (props.done) {
+      return <img alt="Check Done" src="./check-enabled.svg"></img>;
+    } else {
+      return <img alt="Check Off" src="./check-disabled.svg"></img>;
     }
+  }
 
-    return (
-        <ul>
-            {props.items.map(item => 
-            <li 
-            className={item.done ? "done" : ""}
-            key={item.id}> 
-                <button onClick={() => {props.onDoneTask(item)} }>
-                    <DoneImg done={item.done}></DoneImg>
-                </button>
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li className={item.done ? "done" : ""} key={item.id}>
+          <button
+            onClick={() => {
+              props.onDoneTask(item);
+            }}
+          >
+            <DoneImg done={item.done}></DoneImg>
+          </button>
 
-                {item.text}
+          {item.text}
 
-                <button onClick={() => {props.onDeleteTask(item)} }>
-                    <img alt="Delete" src="./trash.png"></img>
-                </button>
-            </li>)}
-        </ul>
-    )
+          <button
+            onClick={() => {
+              props.onDeleteTask(item);
+            }}
+          >
+            <img alt="Delete" src="./trash.svg"></img>
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
